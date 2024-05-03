b8fe2c3c0569a86c17993abe32d4834afe3ee260
tiaguinho <114175416+cod3stealer@users.noreply.github.com>
2024-05-03 17:07:21 +0200

    Estructura base

---
+++ src/KeyManager.java
@@ -0,0 +1,48 @@
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class KeyManager implements KeyListener {
+    // Atributos
+    protected boolean up,down,left,right;
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        switch (e.getKeyCode()) {
+            case KeyEvent.VK_W:
+                up = true;
+                break;
+            case KeyEvent.VK_S:
+                down = true;
+                break;
+            case KeyEvent.VK_A:
+                left = true;
+                break;
+            case KeyEvent.VK_D:
+                right = true;
+                break;
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        switch (e.getKeyCode()) {
+            case KeyEvent.VK_W:
+                up = false;
+                break;
+            case KeyEvent.VK_S:
+                down = false;
+                break;
+            case KeyEvent.VK_D:
+                right = false;
+                break;
+            case KeyEvent.VK_A:
+                left = false;
+                break;
+        }
+    }
+}
