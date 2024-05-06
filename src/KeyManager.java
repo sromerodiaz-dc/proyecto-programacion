e8753a92f20ee8257e55eeaf03f1547fc2e95a0a
tiaguinho <114175416+cod3stealer@users.noreply.github.com>
2024-05-07 00:39:06 +0200

    Comienzo del desarrollo del KeyManager

---
+++ src/KeyManager.java
@@ -0,0 +1,20 @@
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class KeyManager implements KeyListener {
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        // Implementar un switch case en vez de 5 IFs seguidos
+        // switch ()
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+
+    }
+}
