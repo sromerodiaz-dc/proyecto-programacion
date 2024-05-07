130d328eed21ac309ac469e3e8500fbb62a824c0
tiaguinho <114175416+cod3stealer@users.noreply.github.com>
2024-05-07 14:25:44 +0200

    Cambio de instancias, ahora Main instancia unicamente Ventana.

--- src/Main.java
+++ src/Main.java
@@ -8,14 +8,6 @@
 
 public class Main {
     public static void main(String[] args) {
-        TeisPanel teisPanel = new TeisPanel();
-        ventana w = new ventana();
-
-        // Añado al JFrame un componente nuevo, en este caso, "teisPanel" que es un panel de la clase TeisPanel
-        // El "pack()" ajusta la ventana a la resolucion determinada (preferred size) y layouts de sus componentes, en este caso, TeisPanel.
-        w.add(teisPanel);
-        w.pack();
-
-        w.setVisible(true);
+        new ventana();
     }
 }
