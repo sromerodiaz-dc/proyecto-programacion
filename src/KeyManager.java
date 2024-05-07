f9d2c9273e16c997d72249ef1ee135bd61de1c61
tiaguinho <114175416+cod3stealer@users.noreply.github.com>
2024-05-07 12:50:47 +0200

    metodo keyPressed

--- src/KeyManager.java
+++ src/KeyManager.java
@@ -2,6 +2,8 @@ import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
 public class KeyManager implements KeyListener {
+    // Atributos
+    private boolean up,down,left,right;
     @Override
     public void keyTyped(KeyEvent e) {
 
@@ -10,7 +12,17 @@ public class KeyManager implements KeyListener {
     @Override
     public void keyPressed(KeyEvent e) {
         // Implementar un switch case en vez de 5 IFs seguidos
-        // switch ()
+        switch (e.getKeyCode()) {
+            case KeyEvent.VK_W:
+                up = true;
+                break;
+            case KeyEvent.VK_S:
+                down = true;
+            case KeyEvent.VK_A:
+                left = true;
+            case KeyEvent.VK_D:
+                right = true;
+        }
     }
 
     @Override
