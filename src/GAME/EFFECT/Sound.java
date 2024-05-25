d6e30995cc312576f994f644342c60fd54135de0
tiaguinho <114175416+cod3stealer@users.noreply.github.com>
2024-05-25 21:44:26 +0200

    JavaDoc

--- src/GAME/EFFECT/Sound.java
+++ src/GAME/EFFECT/Sound.java
@@ -4,34 +4,72 @@ import javax.sound.sampled.*;
 import java.io.IOException;
 import java.net.URL;
 
+/**
+ * @author Santiago Agustin Romero Diaz
+ * CFP Daniel Castelao
+ * Proyecto: Teis
+ * -
+ * Clase que maneja la reproducción de sonidos en el programa.
+ */
 public class Sound {
+    /**
+     * Clip de audio que se utiliza para reproducir los sonidos.
+     */
     Clip clip;
+    /**
+     * Arreglo de URLs que contienen los paths de los archivos de sonido.
+     */
     URL[] soundPath = new URL[10];
 
+    /**
+     * Constructor de la clase Sound.
+     */
     public Sound() {
+        // Se establecen los paths de los archivos de sonido en el arreglo soundPath.
         soundPath[0] = getClass().getClassLoader().getResource("effects/chillbeat.wav");
         soundPath[1] = getClass().getClassLoader().getResource("effects/recogeobjetos.wav");
         soundPath[2] = getClass().getClassLoader().getResource("effects/normalito.wav");
     }
 
+    /**
+     * Establece el archivo de sonido que se va a reproducir.
+     * @param i Índice del archivo de sonido que se va a reproducir.
+     * @throws LineUnavailableException Si no se puede abrir la línea de audio.
+     */
     public void setFile(int i) throws LineUnavailableException {
         try {
+            // Se crea un AudioInputStream a partir del archivo de sonido seleccionado.
             AudioInputStream ais = AudioSystem.getAudioInputStream(soundPath[i]);
+            // Se crea un Clip de audio y se abre con el AudioInputStream.
             clip = AudioSystem.getClip();
             clip.open(ais);
         } catch (UnsupportedAudioFileException | IOException e) {
+            // Se lanza una excepción RuntimeException si ocurre un error al abrir el archivo de sonido.
             throw new RuntimeException(e);
         }
     }
+
+    /**
+     * Reproduce el sonido actual.
+     */
     public void play(){
+        // Se inicia la reproducción del sonido.
         clip.start();
     }
 
+    /**
+     * Reproduce el sonido en bucle continuo.
+     */
     public void loop(){
+        // Se establece el modo de reproducción en bucle continuo.
         clip.loop(Clip.LOOP_CONTINUOUSLY);
     }
 
+    /**
+     * Detiene la reproducción del sonido.
+     */
     public void stop(){
+        // Se detiene la reproducción del sonido.
         clip.stop();
     }
 }
--- src/GAME/EFFECT/UserInterface.java
+++ src/GAME/EFFECT/UserInterface.java
@@ -6,27 +6,58 @@ import GAME.OBJECT.OBJS.Passvigo;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 
+/**
+ * @author Santiago Agustin Romero Diaz
+ * CFP Daniel Castelao
+ * Proyecto: Teis
+ * -
+ * Clase que representa la interfaz de usuario del programa.
+ */
 public class UserInterface {
-
     // Panel y Fuente de texto
+    /**
+     * Referencia al panel donde se dibujará la interfaz de usuario.
+     */
     TeisPanel teisPanel;
+    /**
+     * Fuente de texto utilizada para dibujar texto en la interfaz de usuario.
+     */
     Font font;
 
     // Implementación de imágen para la UI y acompañar al texto
+    /**
+     * Imagen utilizada para acompañar al texto en la interfaz de usuario.
+     */
     BufferedImage imagePass;
 
+    /**
+     * Constructor de la clase UserInterface.
+     * @param teisPanel Panel donde se dibujará la interfaz de usuario.
+     */
     public UserInterface(TeisPanel teisPanel) {
         this.teisPanel = teisPanel;
-        font = new Font("Arial",Font.ITALIC,40);
+        // Se crea una fuente de texto Arial de 20 puntos y estilo plano.
+        font = new Font("Arial", Font.PLAIN, 20);
+        // Se crea una instancia de la clase Passvigo para obtener la imagen.
         Passvigo passvigo = new Passvigo();
+        // Se asigna la imagen obtenida a la variable imagePass.
         imagePass = passvigo.image;
     }
 
-    // Metodos de dibujado
+    /**
+     * Método que dibuja la interfaz de usuario en el Graphics2D proporcionado.
+     * @param g2 Graphics2D donde se dibujará la interfaz de usuario.
+     */
     public void draw(Graphics2D g2) {
+        // Se establece la fuente de texto y el color blanco para dibujar texto.
         g2.setFont(font);
-        g2.setColor(Color.YELLOW);
-        g2.drawImage(imagePass, teisPanel.sizeFinal/2, teisPanel.sizeFinal/2, teisPanel.sizeFinal*2, teisPanel.sizeFinal*2, null);
-        g2.drawString(teisPanel.model.tenPassVigo, 100, 80);
+        g2.setColor(Color.white);
+        // Se dibuja el texto "tenPassVigo" en la posición (130, 80).
+        g2.drawString(teisPanel.model.tenPassVigo, 130, 80);
+        // Se verifica si la condición "tenPass" es verdadera.
+        if (teisPanel.model.tenPass) {
+            // Si es verdadera, se dibuja la imagen en la posición y tamaño especificados.
+            g2.drawImage(imagePass, teisPanel.sizeFinal/2, teisPanel.sizeFinal/3, teisPanel.sizeFinal*2, teisPanel.sizeFinal*2, null);
+        }
     }
 }
