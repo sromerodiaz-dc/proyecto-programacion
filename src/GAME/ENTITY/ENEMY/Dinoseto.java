dc6d05cb395dcb4a79049db029b2c2ccd073a657
tiaguinho <114175416+cod3stealer@users.noreply.github.com>
2024-05-30 17:47:01 +0200

    PRIMERO ENEMIGO: Dinoseto Elegante

---
+++ src/GAME/ENTITY/ENEMY/Dinoseto.java
@@ -0,0 +1,10 @@
+package GAME.ENTITY.ENEMY;
+
+import GAME.ENTITY.Entity;
+import GAME.GAME.TeisPanel;
+
+public class Dinoseto extends Entity {
+    public Dinoseto (TeisPanel teisPanel) {
+        super(teisPanel);
+    }
+}
