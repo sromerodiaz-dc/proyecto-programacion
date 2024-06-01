a9dbb0eed00a5c6e838cc3dff400de6b00409389
tiaguinho <114175416+cod3stealer@users.noreply.github.com>
2024-06-01 20:39:20 +0200

    Método de dibujado mejorado, solo una llamada a un método dentro de draw()

--- src/GAME/ENTITY/Entity.java
+++ src/GAME/ENTITY/Entity.java
@@ -1,13 +1,15 @@
 package GAME.ENTITY;
 
 import GAME.GAME.TeisPanel;
-import GAME.GPHICS.DrawUtils;
 import GAME.GPHICS.PiezaUtils;
 
 import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
 
 /**
  * @author Santiago Agustin Romero Diaz
@@ -19,7 +21,6 @@ import java.io.IOException;
  * */
 public class Entity {
     TeisPanel teisPanel;
-    public DrawUtils drawUtils = new DrawUtils(); // Clase auxiliar de métodos generales
 
     // Atributos
     public int speed;
@@ -119,19 +120,6 @@ public class Entity {
     }
 
     /**
-     * Dibuja la entidad en el panel utilizando los métodos de dibujo proporcionados.
-     *
-     * @param g2 objeto Graphics2D para dibujar la entidad
-     */
-    public void draw (Graphics2D g2) {
-        // Obtiene la imagen de sprite correspondiente al número de sprite activo y la dirección actual
-        DrawUtils.Pair<BufferedImage, Integer, Integer> result = teisPanel.controller.drawUtils.drawMovement(spriteNum,sentido,stop,stop2,up1,up2,down1,down2,left1,left2,right1,right2,attackUp,attackLeft,attackRight,attackDown,attack,0,0);
-
-        // Dibuja la imagen en la posición relativa al jugador en el panel
-        drawUtils.drawRelativeToPlayer(result.second,result.third,worldX,worldY,teisPanel,g2,result.first, invencible);
-    }
-
-    /**
      * Método que se encarga de establecer eventos específicos para cada entidad.
      * Debe ser sobreescrito en las clases hijas para implementar la lógica de eventos específica.
      */
@@ -210,7 +198,7 @@ public class Entity {
         // Si no hay colisión, mueve al jugador
         if (!collisionOn && !teisPanel.model.keyManager.isPressed) {
             // Calcula la nueva posición del jugador según la dirección y velocidad
-            int[] moveEnt = drawUtils.moveEntity(sentido, worldX, worldY, speed);
+            int[] moveEnt = moveEntity(speed);
 
             // Actualiza la posición del jugador
             worldX = moveEnt[0];
@@ -218,10 +206,138 @@ public class Entity {
         }
 
         // Actualiza el contador de sprites y cambia el spriteNum si es necesario
-        int[] spritesCount = drawUtils.updateSpriteCounter(spriteNum, spriteCounter, intervalo);
+        int[] spritesCount = updateSpriteCounter();
 
         // Actualiza el contador de sprites y el spriteNum
         spriteNum = spritesCount[0];
         spriteCounter = spritesCount[1];
+    }
+
+    /**
+     * Dibuja la entidad en el panel utilizando los métodos de dibujo proporcionados.
+     *
+     * @param g2 objeto Graphics2D para dibujar la entidad
+     */
+    public void draw (Graphics2D g2) {
+        // Obtiene la imagen de sprite correspondiente al número de sprite activo y la dirección actual
+        // Dibuja la imagen en la posición relativa al jugador en el panel
+        drawMovement(g2);
+    }
+
+    public void drawMovement(Graphics2D g2) {
+        // Crea un mapa que asocia cada dirección con un array de imágenes de sprite
+        Map<Character, BufferedImage[]> sprites = new HashMap<>();
+        sprites = fillSprites(sprites,stop,stop2,up1,up2,down1,down2,left1,left2,right1,right2);
+
+        // Obtiene el array de imágenes de sprite correspondiente a la dirección actual
+        BufferedImage[] images = sprites.get(sentido);
+
+        // Obtiene la imagen de sprite correspondiente al número de sprite activo
+
+        drawRelativeToPlayer(worldX,worldY, teisPanel, g2,images[spriteNum - 1], invencible);
+    }
+
+    /**
+     * Método generico de dibujado de spritems
+     * */
+    public Map<Character, BufferedImage[]> fillSprites(Map<Character, BufferedImage[]> sprites, BufferedImage stop, BufferedImage stop2, BufferedImage up1, BufferedImage up2, BufferedImage down1, BufferedImage down2, BufferedImage left1, BufferedImage left2, BufferedImage right1, BufferedImage right2) {
+        sprites.put('w', new BufferedImage[]{up1, up2});
+        sprites.put('a', new BufferedImage[]{left1, left2});
+        sprites.put('s', new BufferedImage[]{down1, down2});
+        sprites.put('d', new BufferedImage[]{right1, right2});
+        sprites.put('0', new BufferedImage[]{stop, stop2});
+        return sprites;
+    }
+
+    /**
+     * Dibuja una imagen en una posición relativa al jugador en un panel.
+     *
+     * @param worldX coordenada X del mundo
+     * @param worldY coordenada Y del mundo
+     * @param teisPanel panel donde se dibujará la imagen
+     * @param g2 objeto Graphics2D para dibujar la imagen
+     * @param image imagen a dibujar
+     */
+    public void drawRelativeToPlayer(int worldX, int worldY, TeisPanel teisPanel, Graphics2D g2, BufferedImage image, boolean invencible) {
+        // Calcula las coordenadas de pantalla a partir de worldX e worldY
+        int screenX = worldX - teisPanel.model.worldX + teisPanel.model.screenX;
+        int screenY = worldY - teisPanel.model.worldY + teisPanel.model.screenY;
+
+
+        // Verifica si la imagen está dentro de la pantalla
+        if (worldX + teisPanel.sizeFinal > teisPanel.model.worldX - teisPanel.model.screenX &&
+                worldX - teisPanel.sizeFinal < teisPanel.model.worldX + teisPanel.model.screenX &&
+                worldY + teisPanel.sizeFinal > teisPanel.model.worldY - teisPanel.model.screenY &&
+                worldY - teisPanel.sizeFinal < teisPanel.model.worldY + teisPanel.model.screenY) {
+            // Dibuja la imagen en la posición correspondiente
+            if (invencible) g2.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER,0.4f));
+            g2.drawImage(image, screenX, screenY, null);
+            g2.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER,1f));
+        }
+    }
+
+    /**
+     * Mueve a la entidad según la dirección del movimiento.
+     */
+    public int[] moveEntity(int speed) {
+        int[] coordenadas = new int[2];
+        // Mueve al jugador según la dirección del movimiento
+        switch (sentido) {
+            case 'w':
+                worldY -= speed;
+                break;
+            case 's':
+                worldY += speed;
+                break;
+            case 'a':
+                worldX -= speed;
+                break;
+            case 'd':
+                worldX += speed;
+                break;
+        }
+        coordenadas[0] = worldX;
+        coordenadas[1] = worldY;
+        return coordenadas;
+    }
+
+    /**
+     * Método que devuelve una dirección aleatoria para mover una entidad.
+     * La dirección se elige aleatoriamente entre 'w', '', 'a' y 'd'.
+     *
+     * @return una dirección aleatoria como carácter ('w', '', 'a' o 'd')
+     */
+    public char moveRandomEntity(){
+        Random random = new Random();
+        int i = random.nextInt(100)+1; // Genera un número aleatorio entre 1 y 100
+
+        // Asigna una dirección aleatoria según el rango del número generado
+        if (i < 23) return 'w'; // 23% de probabilidad de moverse hacia arriba
+        else if (i < 46) return 's'; // 23% de probabilidad de moverse hacia abajo
+        else if (i < 69) return 'a'; // 23% de probabilidad de moverse hacia abajo
+        else if (i < 92) return 'd'; // 23% de probabilidad de moverse hacia la derecha
+        else return '0'; // 8% de probabilidad de quedarse quiero
+    }
+
+    /**
+     * Método que actualiza el contador de sprites y cambia el spriteNum si es necesario.
+     *
+     * @return un array con el nuevo spriteNum y el nuevo contador de sprites
+     */
+    public int[] updateSpriteCounter() {
+        int[] counters = new int[2];
+
+        // Incrementa el contador de sprites
+        spriteCounter++;
+
+        // Si el contador de sprites es mayor a 15, cambia el spriteNum y resetea el contador
+        if (spriteCounter > intervalo) {
+            spriteNum = (spriteNum == 1)? 2 : 1; // Cambia el spriteNum entre 1 y 2
+            spriteCounter = 0; // Resetea el contador de sprites
+        }
+
+        counters[0] = spriteNum;
+        counters[1] = spriteCounter;
+        return counters;
     }
 }
