fa0c72f11caa2c328a7b44ead1b7992db3507e9e
Santiago <114175416+sromerodiaz-dc@users.noreply.github.com>
2025-04-20 16:30:31 +0200

    Exception adaptado

--- src/main/java/com/game/Main.java
+++ src/main/java/com/game/Main.java
@@ -2,6 +2,8 @@ package com.game;
 
 import com.game.controller.ventana;
 
+import javax.sound.sampled.LineUnavailableException;
+
 /**
  * Clase main se encarga de lanzar el programa
  *
@@ -10,7 +12,7 @@ import com.game.controller.ventana;
  * Proyecto: Teis
  * */
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws LineUnavailableException {
         new ventana();
     }
 }
