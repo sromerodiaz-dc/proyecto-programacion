681beb479ffbf7ab83123417834a2bb9ab0b3080
sromero.ms <electronica1@merasys.tech>
2025-04-21 12:42:08 +0200

    Clase representativa de datos de eventos

---
+++ src/main/java/com/game/controller/events/GameEvent.java
@@ -0,0 +1,8 @@
+package com.game.controller.events;
+
+/**
+ * Clase inmutable y concisa que se usa para representar datos sin necesidad de escribir
+ * el código boilerplate típico de los POJOs (constructores, getters, equals, hashCode, toString, etc.).
+ */
+public record GameEvent(EventType type, int col, int row, char direction, String message, int value) {
+}
