a0dd2c752804a2ca9a2021a0b5c948ef4a63db80
sromero.ms <electronica1@merasys.tech>
2025-04-22 13:47:02 +0200

    objetos de combate añadidos

---
+++ src/main/java/com/game/entity/object/Shield.java
@@ -0,0 +1,20 @@
+package com.game.entity.object;
+
+import com.game.controller.TeisPanel;
+import com.game.data.Properties;
+import com.game.entity.Entity;
+
+public class Shield extends Entity {
+    /**
+     * Constructor parametrizado
+     *
+     * @param teisPanel  gráfico del juego
+     */
+    public Shield(TeisPanel teisPanel, Properties properties) {
+        super(teisPanel, properties);
+
+        name = "Fent";
+        //down1 = setEntitySprite()
+        defenseVal = 1;
+    }
+}
---
+++ src/main/java/com/game/entity/object/Weapon.java
@@ -0,0 +1,20 @@
+package com.game.entity.object;
+
+import com.game.controller.TeisPanel;
+import com.game.data.Properties;
+import com.game.entity.Entity;
+
+public class Weapon extends Entity {
+    /**
+     * Constructor parametrizado
+     *
+     * @param teisPanel  gráfico del juego
+     */
+    public Weapon(TeisPanel teisPanel, Properties properties) {
+        super(teisPanel, properties);
+
+        name = "Corporative BarberCut";
+        //down1 = setEntitySprite()
+        attackVal = 1;
+    }
+}
