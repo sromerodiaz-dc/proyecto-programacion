ebea353f6fa73dc84652a8eab37439638b8ba770
sromero-dc <sromerodiaz@danielcastelao.org>
2025-05-05 09:44:19 +0200

    clase manager para los sonidos por desarrollar

---
+++ src/main/java/com/game/efx/SoundManager.java
@@ -0,0 +1,11 @@
+package com.game.efx;
+
+import com.game.controller.events.EventListener;
+import com.game.controller.events.GameEvent;
+
+public class SoundManager implements EventListener {
+    @Override
+    public void onEvent(GameEvent event) {
+
+    }
+}
