86898477c44ad5a7177a3ec9e816c10b79a2025b
sromero-dc <sromerodiaz@danielcastelao.org>
2025-05-15 13:31:45 +0200

    clase auxiliar

---
+++ src/main/java/com/editorv2/util/StartAction.java
@@ -0,0 +1,24 @@
+// StartAction.java (nuevo archivo)
+package com.editorv2.util;
+
+public class StartAction {
+    public enum ActionType {CREATE, LOAD}
+    public final ActionType type;
+    public final int rows;
+    public final int cols;
+    public final String mapName;
+
+    public StartAction(ActionType type, int rows, int cols) {
+        this.type = type;
+        this.rows = rows;
+        this.cols = cols;
+        this.mapName = null;
+    }
+
+    public StartAction(ActionType type, String mapName) {
+        this.type = type;
+        this.mapName = mapName;
+        this.rows = 0;
+        this.cols = 0;
+    }
+}
