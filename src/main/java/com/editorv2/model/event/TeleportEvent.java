075a7f87aa843eae5a17a564ff2ac53698056692
sromero-dc <sromerodiaz@danielcastelao.org>
2025-05-16 12:58:47 +0200

    coordenadas target

--- src/main/java/com/editorv2/model/event/TeleportEvent.java
+++ src/main/java/com/editorv2/model/event/TeleportEvent.java
@@ -3,16 +3,19 @@ package com.editorv2.model.event;
 public class TeleportEvent extends MapEvent {
     private final int row;
     private final int col;
-    private final int[] targetXY;
+    private final int targetRow;
+    private final int targetCol;
 
-    public TeleportEvent(int row, int col, int[] targetXY) {
+    public TeleportEvent(int row, int col, int targetX, int targetY) {
         super("teleport");
         this.row = row;
         this.col = col;
-        this.targetXY = targetXY;
+        this.targetRow = targetY;
+        this.targetCol = targetX;
     }
 
     public int getRow() { return row; }
     public int getCol() { return col; }
-    public int[] getTargetXY() { return targetXY; }
+    public int getTargetRow() { return targetRow; }
+    public int getTargetCol() { return targetCol; }
 }
