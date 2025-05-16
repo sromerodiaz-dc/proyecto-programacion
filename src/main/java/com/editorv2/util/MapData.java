8bfa25a154ff23f601dbbf2f7e67dbc54019d439
sromero-dc <sromerodiaz@danielcastelao.org>
2025-05-16 12:57:29 +0200

    añadidos eventos

--- src/main/java/com/editorv2/util/MapData.java
+++ src/main/java/com/editorv2/util/MapData.java
@@ -1,3 +1,10 @@
 package com.editorv2.util;
 
-public record MapData(int rows, int cols, int[][] matrix) {}
+import com.editorv2.model.CeldaCoord;
+import com.editorv2.model.event.SpawnEvent;
+import com.editorv2.model.event.TeleportEvent;
+
+import java.util.List;
+import java.util.Set;
+
+public record MapData(int rows, int cols, int[][] matrix, Set<CeldaCoord> collisions, SpawnEvent spawnEvent, List<TeleportEvent> teleports) {}
