9eb1515a7a6ce4443af840266284769511fb8861
sromero-dc <sromerodiaz@danielcastelao.org>
2025-06-10 14:46:54 +0200

    revisión de //TODOs

---
+++ src/main/java/com/editor/view/EventView.java
@@ -0,0 +1,10 @@
+package com.editor.view;
+
+public class EventView {
+    //TODO mejorar el panel de los eventos
+    // La idea es que se puedan poner sprites en aquellos lugares.
+    // Ejemplo:
+    // Evento de daño en X, Y donde se posiciona un periodico
+    // Al momento de recogerlo entonces se lee el periodico y al jugador se le quita vida
+    // La idea es poder dejar programado el evento desde el propio editor
+}
