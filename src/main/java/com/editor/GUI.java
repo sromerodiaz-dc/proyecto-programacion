ff237bb00f7983ab3ffbb5bd3e37bb258f1305d0
sromero-dc <sromerodiaz@danielcastelao.org>
2025-06-11 10:06:38 +0200

    nuevos //TODOs

--- src/main/java/com/editor/GUI.java
+++ src/main/java/com/editor/GUI.java
@@ -26,7 +26,16 @@ import static com.editor.util.MapJsonHandler.showError;
 import static com.editor.util.MapJsonHandler.showInformation;
 
 public class GUI extends JFrame {
-
+    //TODO boton para pintar todo el fondo (util para escenarios de exterior)
+    //TODO añadir bloques de sprites (Ejemplo: dibujar un edificio requiere 6 tiles diferentes, pues automatizarlo)
+    //TODO arreglar colisiones. Que se guarden en el array al momento de seleccionar un tile y pintar los tiles ya pintados.
+    // Que se puedan quitar colisiones con clic derecho habiendo seleccionado antes el boton de colision mode
+    // Las colisiones que se quiten que se pinten de manera normal de nuevo
+    // Si un tile X es pintado y luego seleccionado como colisionable:
+    // - Los tiles pintados se pintarán como colisionables
+    // - Los tiles repintados como colisionables son individuales e independientes, esto quiere decir que se
+    // puede quitar el modo colisionable haciendo clic derecho en edicion modo colision
+    //TODO teclado aplicado al programa para no tener que hacer clic para nada
     private static final String CONFIG_PATH = "src/main/resources/tiles.json";
     private static final Dimension WINDOW_SIZE = new Dimension(1600, 900);
     private static final Color BACKGROUND_COLOR = Color.BLACK;
